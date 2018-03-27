2d60d822d878a2087e8c674ffcbd8c572b88d37e
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-03-26 21:04:55 -0700

    Create and connect to websocket

--- server/app.js
+++ server/app.js
@@ -1,11 +1,13 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 
 const publicPath = path.join(__dirname, '..', '/public');
 
 // Static Files Middleware
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(publicPath));
 
 // View Engine Middleware
 app.set('views', path.join(__dirname, '..', '/views'));
@@ -17,4 +19,7 @@ app.get('/', (req, res) => {
 
 // Listen to Server
 const port = 3000;
-app.listen(port, () => { console.log(`Now listening to Port:${port}`) });
+
+io.on('connection', () => { console.log('socket') });
+server.listen(port, () => { console.log('now listening to port 3000') });
+// app.listen(port, () => { console.log(`Now listening to Port: ${port}`) });
